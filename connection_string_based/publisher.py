f5e2dd62356a6be805044e039a2f3c64e31479b4
Mazhar <princemazhar.mp@gmail.com>
2024-06-01 14:35:44 +0600

    Initialize with basic config

---
+++ connection_string_based/publisher.py
@@ -0,0 +1,15 @@
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from azure.servicebus.exceptions import ServiceBusError, OperationTimeoutError
+
+import logging
+from dotenv import load_dotenv
+import os
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+connection_str = os.getenv('CONNECTION_STR')
+topic_name = os.getenv('TOPIC_NAME')
+max_retries = 3
