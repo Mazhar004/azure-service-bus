fc55c4d6c18018216b748899fce64586336a13e0
Mazhar <princemazhar.mp@gmail.com>
2024-06-01 20:42:10 +0600

    Code formatting and add type hint

--- utils/azure_monitor.py
+++ utils/azure_monitor.py
@@ -1,20 +1,18 @@
-import enum
-import json
 import logging
-import os
+from typing import Any, Dict
 
 from pythonjsonlogger import jsonlogger
 
-DEFAULT_LOG_LEVEL = logging.INFO
-DEFAULT_LOG_FORMAT = '%(levelname)s %(asctime)s %(module)s %(message)s'
+DEFAULT_LOG_LEVEL: int = logging.INFO
+DEFAULT_LOG_FORMAT: str = '%(levelname)s %(asctime)s %(module)s %(message)s'
 
 
 class AzureJsonFormatter(jsonlogger.JsonFormatter):
-    def __init__(self, fmt=DEFAULT_LOG_FORMAT, style='%', *args, **kwargs):
-        jsonlogger.JsonFormatter.__init__(self, fmt=fmt, *args, **kwargs)
+    def __init__(self, fmt: str = DEFAULT_LOG_FORMAT, style: str = '%', *args: Any, **kwargs: Any) -> None:
+        super().__init__(fmt=fmt, *args, **kwargs)
 
-    def process_log_record(self, record):
-        log_dict = dict(record)
+    def process_log_record(self, record: Dict[str, Any]) -> Dict[str, Any]:
+        log_dict: Dict[str, Any] = dict(record)
 
         log_dict['severity'] = log_dict['levelname']
         log_dict['logger'] = log_dict.get('name')
@@ -22,11 +20,10 @@ class AzureJsonFormatter(jsonlogger.JsonFormatter):
         if 'exc_info' in log_dict:
             log_dict['exc_info'] = str(log_dict['exc_info'])
 
-        return super(AzureJsonFormatter, self).process_log_record(log_dict)
+        return super().process_log_record(log_dict)
 
 
-def configure_logging():
-    # Get the root logger
+def configure_logging() -> logging.Logger:
     default_logger = logging.getLogger(__name__)
     default_logger.setLevel(DEFAULT_LOG_LEVEL)
     default_logger.handlers = []
--- utils/basic_logging.py
+++ utils/basic_logging.py
@@ -1,9 +1,9 @@
 import logging
 
 
-def configure_logging():
-    default_log_level = logging.INFO
-    default_log_format = '%(levelname)s %(asctime)s %(module)s %(message)s'
+def configure_logging() -> logging.Logger:
+    default_log_level: int = logging.INFO
+    default_log_format: str = '%(levelname)s %(asctime)s %(module)s %(message)s'
 
     # Get the root logger
     default_logger = logging.getLogger(__name__)
@@ -13,5 +13,4 @@ def configure_logging():
     stream_logger = logging.StreamHandler()
     stream_logger.setFormatter(logging.Formatter(default_log_format))
     default_logger.addHandler(stream_logger)
-
     return default_logger
