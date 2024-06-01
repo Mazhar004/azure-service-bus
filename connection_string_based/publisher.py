8e38a7487920442b0f7736ef1a15ca4c8b2be6bb
Mazhar <princemazhar.mp@gmail.com>
2024-06-01 14:55:22 +0600

    Organize import and add type hint

--- connection_string_based/publisher.py
+++ connection_string_based/publisher.py
@@ -1,28 +1,31 @@
-from azure.servicebus import ServiceBusClient, ServiceBusMessage
-from azure.servicebus.exceptions import ServiceBusError, OperationTimeoutError
-
+import argparse
 import logging
-from dotenv import load_dotenv
 import os
+import time
+from typing import Any
 
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from azure.servicebus.exceptions import OperationTimeoutError, ServiceBusError
+from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 load_dotenv()
-CONNECTION_STR = os.getenv('CONNECTION_STR')
-TOPIC_NAME = os.getenv('TOPIC_NAME')
+CONNECTION_STR: str = os.getenv('CONNECTION_STR')
+TOPIC_NAME: str = os.getenv('TOPIC_NAME')
+
 
 class Publisher:
-    def __init__(self, connection_str, topic_name, max_retries=3, retry_delay=5):
+    def __init__(self, connection_str: str, topic_name: str, max_retries: int = 3, retry_delay: int = 5):
         self._connection_str = connection_str
         self.topic_name = topic_name
         self.max_retries = max_retries
         self.retry_delay = retry_delay
 
         self.servicebus_client = ServiceBusClient.from_connection_string(conn_str=self._connection_str)
-        
-    def publish(self, data):
+
+    def publish(self, data: Any) -> None:
         with self.servicebus_client.get_topic_sender(self.topic_name) as sender:
             for attempt in range(self.max_retries):
                 try:
@@ -32,6 +35,7 @@ class Publisher:
                     break
                 except (ServiceBusError, OperationTimeoutError) as e:
                     logger.warning("Attempt %d: Failed to publish message: %s", attempt + 1, e)
+                    
                     if attempt < self.max_retries - 1:
                         time.sleep(self.retry_delay)
                     else:
@@ -43,9 +47,12 @@ class Publisher:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Publish a message to a Service Bus topic.")
-    parser.add_argument("--msg", type=str, required=True, help="The message to publish.")
-    
+    parser.add_argument("--msg",
+                        type=str,
+                        required=True,
+                        help="The message to publish.")
+
     args = parser.parse_args()
-    
+
     publisher = Publisher(CONNECTION_STR, TOPIC_NAME)
     publisher.publish(args.msg)
