726de5667d97d947be9dc9abc1f286b8e4a070f4
Mazhar <princemazhar.mp@gmail.com>
2024-06-01 14:40:31 +0600

    Use class based approach

--- connection_string_based/publisher.py
+++ connection_string_based/publisher.py
@@ -16,22 +16,31 @@ max_retries = 3
 
 servicebus_client = ServiceBusClient.from_connection_string(conn_str=connection_str)
 
-retry_delay = 5
+class Publisher:
+    def __init__(self, servicebus_client, topic_name, max_retries=3, retry_delay=5):
+        self.servicebus_client = servicebus_client
+        self.topic_name = topic_name
+        self.max_retries = max_retries
+        self.retry_delay = retry_delay
 
-with servicebus_client.get_topic_sender(topic_name) as sender:
-    for attempt in range(max_retries):
-        try:
-            message = ServiceBusMessage(data)
-            sender.send_messages(message)
-            logger.info("Message published successfully on attempt %d", attempt + 1)
-            break
-        except (ServiceBusError, OperationTimeoutError) as e:
-            logger.warning("Attempt %d: Failed to publish message: %s", attempt + 1, e)
-            if attempt < max_retries - 1:
-                time.sleep(retry_delay)
-            else:
-                logger.error("Exceeded max retries. Failed to publish message after %d attempts.", max_retries)
-                raise e
+    def publish(self, data):
+        with self.servicebus_client.get_topic_sender(self.topic_name) as sender:
+            for attempt in range(self.max_retries):
+                try:
+                    message = ServiceBusMessage(data)
+                    sender.send_messages(message)
+                    logger.info("Message published successfully on attempt %d", attempt + 1)
+                    break
+                except (ServiceBusError, OperationTimeoutError) as e:
+                    logger.warning("Attempt %d: Failed to publish message: %s", attempt + 1, e)
+                    if attempt < self.max_retries - 1:
+                        time.sleep(self.retry_delay)
+                    else:
+                        logger.error("Exceeded max retries. Failed to publish message after %d attempts.", self.max_retries)
+                        raise e
+
+        print("Message published.")
 
 
-print("Message published.")
+publisher = Publisher(servicebus_client, topic_name)
+publisher.publish(data)
