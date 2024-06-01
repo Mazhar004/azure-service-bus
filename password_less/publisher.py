d4c8d66255812ca891d26043b099a9f314a44e9c
Mazhar <princemazhar.mp@gmail.com>
2024-06-01 20:15:28 +0600

    Async message publisher

--- password_less/publisher.py
+++ password_less/publisher.py
@@ -23,6 +23,22 @@ class Publisher:
         self.topic_name = topic_name
         self.credential = DefaultAzureCredential()
 
+    async def publish(self, message_body: str) -> None:
+        try:
+            message = ServiceBusMessage(message_body)
+
+            servicebus_client = ServiceBusClient(self.fully_qualified_namespace,
+                                                 self.credential)
+
+            sender = servicebus_client.get_topic_sender(topic_name=self.topic_name)
+
+            await sender.send_messages(message)
+            logger.info(f'Published a message "{message_body}" to the topic "{self.topic_name}"')
+
+        except (ServiceBusError, OperationTimeoutError) as Err:
+            logger.error(f"An error occurred: {Err}", exc_info=True)
+            raise
+
 
 if __name__ == "__main__":
     logger = azure_monitor.configure_logging()
