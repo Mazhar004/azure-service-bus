5c2699f900faf27c1bb0a55af388990cb55450fe
Mazhar <princemazhar.mp@gmail.com>
2024-06-02 12:54:32 +0600

    Create publisher as a global object

--- password_less/publisher.py
+++ password_less/publisher.py
@@ -1,46 +1,20 @@
 import argparse
 import asyncio
 import logging
-import os
 
+# Azure
 from azure.identity.aio import DefaultAzureCredential
-from azure.servicebus import ServiceBusMessage
-from azure.servicebus.aio import ServiceBusClient
-from azure.servicebus.exceptions import OperationTimeoutError, ServiceBusError
-from dotenv import load_dotenv
 
-from utils import azure_monitor
-
-load_dotenv()
-FULLY_QUALIFIED_NAMESPACE = os.getenv('FULLY_QUALIFIED_NAMESPACE')
-TOPIC_NAME = os.getenv('TOPIC_NAME')
+# Customs
+from abstraction import TopicMessageSenderStrategy
+from auth import namespace_name, topic_name
+from pubsub_utils import ServiceBusPublisher
 
 
-class Publisher:
-    def __init__(self, fully_qualified_namespace: str, topic_name: str):
-        self.fully_qualified_namespace = fully_qualified_namespace
-        self.topic_name = topic_name
-        self.credential = DefaultAzureCredential()
+publisher = ServiceBusPublisher(namespace=namespace_name(),
+                                queue_or_topic_name=topic_name(),
+                                strategy=TopicMessageSenderStrategy)
 
-    async def publish(self, message_body: str) -> None:
-        try:
-            message = ServiceBusMessage(message_body)
-
-            servicebus_client = ServiceBusClient(self.fully_qualified_namespace,
-                                                 self.credential)
-
-            sender = servicebus_client.get_topic_sender(topic_name=self.topic_name)
-
-            await sender.send_messages(message)
-            logger.info(f'Published a message "{message_body}" to the topic "{self.topic_name}"')
-
-        except (ServiceBusError, OperationTimeoutError) as Err:
-            logger.error(f"An error occurred: {Err}", exc_info=True)
-            raise
-
-
-if __name__ == "__main__":
-    logger = azure_monitor.configure_logging()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--msg",
