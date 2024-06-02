051233db8b40179ef61be101a4e1a65509b98c56
Mazhar <princemazhar.mp@gmail.com>
2024-06-02 16:13:50 +0600

    Service message subscriber class creation

--- password_less/pubsub_utils.py
+++ password_less/pubsub_utils.py
@@ -1,7 +1,12 @@
-from azure.identity.aio import DefaultAzureCredential
-from abstraction import MessageSenderStrategy, ServiceBusClientFactory
-from typing import List, Union
+from typing import Callable, List, Union
 
+from abstraction import (MessageReceiverStrategy,
+                         MessageSenderStrategy,
+                         ServiceBusClientFactory)
+
+
+from azure.identity.aio import DefaultAzureCredential
+from azure.servicebus import ServiceBusReceivedMessage
 
 class ServiceBusPublisher:
     def __init__(self, namespace: str, queue_or_topic_name: str, strategy: MessageSenderStrategy) -> None:
@@ -19,3 +24,26 @@ class ServiceBusPublisher:
             await self.strategy.send_message(client=self.client,
                                              name=self.queue_or_topic_name,
                                              message_content=message_content)
+
+
+class ServiceBusSubscriber:
+    def __init__(self, namespace: str, queue_or_topic_name: str, strategy: MessageReceiverStrategy) -> None:
+        self.namespace = namespace
+        self.queue_or_topic_name = queue_or_topic_name
+        
+        self.client = None
+        self.strategy: MessageReceiverStrategy = strategy()
+
+    async def start_listening(self,
+                              message_handler: Callable[[ServiceBusReceivedMessage], None],
+                              subscription_name: str = None) -> None:
+
+        if self.client is None:
+            self.client = await ServiceBusClientFactory.get_client(namespace=self.namespace)
+
+        async with self.client:
+            await self.strategy.subscribing_messages(client=self.client,
+                                               name=self.queue_or_topic_name,
+                                               message_handler=message_handler,
+                                               subscription_name=subscription_name)
+        
