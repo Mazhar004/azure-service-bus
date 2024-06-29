50ec23182d6f4450c12b51a89105dd47498048a4
Mazhar Hossain <princemazhar.mp@gmail.com>
2024-06-29 18:16:21 +0600

    Client creation - Add logging - Consider _credential as class variable (To close the credential later) - Add client for string based connection

--- password_less/client/client_abstraction.py
+++ password_less/client/client_abstraction.py
@@ -6,12 +6,25 @@ from azure.servicebus.aio import ServiceBusClient
 
 class ServiceBusClientFactory:
     _instances: Dict[str, ServiceBusClient] = {}
+    _credential = DefaultAzureCredential()
 
     @staticmethod
     async def get_client(namespace: str) -> ServiceBusClient:
-        credential = DefaultAzureCredential()
-
         if namespace not in ServiceBusClientFactory._instances:
             ServiceBusClientFactory._instances[namespace] = ServiceBusClient(fully_qualified_namespace=namespace,
-                                                                             credential=credential)
+                                                                             credential=ServiceBusClientFactory._credential,
+                                                                             logging_enable=True)
         return ServiceBusClientFactory._instances[namespace]
+
+
+class ServiceBusClientFactoryString:
+    _instances: Dict[str, ServiceBusClient] = {}
+
+    def __init__(self, connection_str) -> None:
+        self.connection_str = connection_str
+
+    async def get_client(self, namespace: str) -> ServiceBusClient:
+        if namespace not in ServiceBusClientFactoryString._instances:
+            ServiceBusClientFactoryString._instances[namespace] = ServiceBusClient.from_connection_string(conn_str=self.connection_str,
+                                                                                                          logging_enable=True)
+        return ServiceBusClientFactoryString._instances[namespace]
